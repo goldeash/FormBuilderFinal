5ccf5d63c8d7e0998098194bfe228cd43a4f1ef5
goldeash <82895382+goldeash@users.noreply.github.com>
2025-05-04 22:54:16 +0300

    fixed user delete

--- FormBuilderFinal/Controllers/AdminController.cs
+++ FormBuilderFinal/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using FormBuilder.Models;
+using FormBuilder.Data;
 
 namespace FormBuilder.Controllers
 {
@@ -11,13 +12,16 @@ namespace FormBuilder.Controllers
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
 
         public AdminController(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _context = context;
         }
 
         public async Task<IActionResult> Index()
@@ -109,7 +113,67 @@ namespace FormBuilder.Controllers
 
             var isCurrentUser = user.Id == _userManager.GetUserId(User);
 
-            await _userManager.UpdateSecurityStampAsync(user);
+            var userTemplates = await _context.Templates
+                .Include(t => t.Tags)
+                .Include(t => t.AllowedUsers)
+                .Include(t => t.Questions)
+                    .ThenInclude(q => q.Options)
+                .Include(t => t.Comments)
+                .Include(t => t.Likes)
+                .Where(t => t.UserId == user.Id)
+                .ToListAsync();
+
+            foreach (var template in userTemplates)
+            {
+                var forms = await _context.Forms
+                    .Include(f => f.Answers)
+                    .Where(f => f.TemplateId == template.Id)
+                    .ToListAsync();
+
+                foreach (var form in forms)
+                {
+                    _context.Answers.RemoveRange(form.Answers);
+                    _context.Forms.Remove(form);
+                }
+
+                _context.TemplateTags.RemoveRange(template.Tags);
+                _context.TemplateAccesses.RemoveRange(template.AllowedUsers);
+                _context.Comments.RemoveRange(template.Comments);
+                _context.Likes.RemoveRange(template.Likes);
+
+                foreach (var question in template.Questions)
+                {
+                    _context.Options.RemoveRange(question.Options);
+                }
+                _context.Questions.RemoveRange(template.Questions);
+                _context.Templates.Remove(template);
+            }
+
+            var userForms = await _context.Forms
+                .Include(f => f.Answers)
+                .Where(f => f.UserId == user.Id)
+                .ToListAsync();
+
+            foreach (var form in userForms)
+            {
+                _context.Answers.RemoveRange(form.Answers);
+                _context.Forms.Remove(form);
+            }
+
+            var userLikes = await _context.Likes
+                .Where(l => l.UserId == user.Id)
+                .ToListAsync();
+            _context.Likes.RemoveRange(userLikes);
+
+            var userComments = await _context.Comments
+                .Where(c => c.UserId == user.Id)
+                .ToListAsync();
+            _context.Comments.RemoveRange(userComments);
+
+            var templateAccesses = await _context.TemplateAccesses
+                .Where(ta => ta.UserId == user.Id)
+                .ToListAsync();
+            _context.TemplateAccesses.RemoveRange(templateAccesses);
 
             var result = await _userManager.DeleteAsync(user);
 
